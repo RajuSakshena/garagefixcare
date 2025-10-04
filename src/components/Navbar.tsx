fff142ea3f070bd2d4924253e38eaee608c436c4
Raju Sakshena <jackmyble@gmail.com>
2025-10-04 10:52:41 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -36,7 +36,7 @@ const Navbar = () => {
 
         {/* Main Navbar */}
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 bg-sky-100">
-          <div className="flex justify-between items-center h-20 relative">
+          <div className="flex justify-between items-center h-14 relative">
 
             {/* ✅ Logo + Contact Info Combined */}
             <div className="flex items-center space-x-3 sm:space-x-6">
